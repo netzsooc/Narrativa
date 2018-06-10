edd928b05b8da8a88ece82f63dec6a5ea3165069
Octavio Sánchez <netzsooc@gmail.com>
2018-06-10 16:10:02 -0500

    genera corpus

--- lda_novels.py
+++ lda_novels.py
@@ -6,18 +6,46 @@ from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import word_tokenize as tokenize
 from nltk.stem import WordNetLemmatizer
 
-NOVELS_DIR = 'Limpios'
-novelas = os.listdir(pathlib.Path(NOVELS_DIR))
+def main():
+    NOVELS_DIR = 'Limpios'
+    novelas = os.listdir(pathlib.Path(NOVELS_DIR))
 
-print(novelas)
-stemmer = SnowballStemmer("english")
-lemmer = WordNetLemmatizer()
-sw = stopwords.words('english')
+    stemmer = SnowballStemmer("english")
+    lemmer = WordNetLemmatizer()
+    sw = stopwords.words('english')
+    corpus = open('corpus_total.txt', 'w', encoding='utf8')
+    improcesables = []
+    for novela in novelas:
+        print('procesando {}'.format(novela))
+        novela_path = pathlib.Path(NOVELS_DIR, novela)
+        
+        try:
+            titulo = novela_path.stem
+            with open(novela_path) as f:
+                libro = f.read()
+                libro = libro.strip().split()
+                libro = ' '.join(libro)
+                tokens = (w for w in tokenize(libro) if w not in sw)
+                lemas = (lemmer.lemmatize(tok).lower() for tok in tokens
+                         if tok.isalpha())
+                tokenizada = ' '.join(lemma for lemma in lemas).strip()
+#                tokenizada = ' '.join(x for x in novel_words(f, lemmer, sw))
+            
+            corpus.write('{} {}\n'.format(titulo, tokenizada))
+        except UnicodeDecodeError:
+            improcesables.append(novela)
+    corpus.close()
+    # print(tokenizada)
+    # print(improcesables)
 
-
-def novel_words (novel, lemmatizer, sw):
+def novel_words (novel, lemmer, sw):
     for line in novel:
-        line = line.strip()
+        line = ' '.join(line.split()).strip()
         tokens = (w for w in tokenize(line) if w not in sw)
-        nuevos = (lemmer.lemmatize(tok) for tok in tokens if tok.isalpha())
+        nuevos = (lemmer.lemmatize(tok).lower() for tok in tokens
+                  if tok.isalpha())
         yield ' '.join(nuevos)
+
+
+if __name__ == '__main__':
+    main()
