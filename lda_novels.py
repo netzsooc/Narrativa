954ddd77d4a6ed8016d300a894733975a35b56b4
Octavio Sánchez <netzsooc@gmail.com>
2018-06-10 14:43:43 -0500

    First commit

--- lda_novels.py
+++ lda_novels.py
@@ -1,12 +1,23 @@
+import os
+import pathlib
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
-from nltk.tokenize.stanford import StanfordTokenizer
+#from nltk.tokenize.stanford import StanfordTokenizer
+from nltk.tokenize import word_tokenize as tokenize
+from nltk.stem import WordNetLemmatizer
 
+NOVELS_DIR = 'Limpios'
+novelas = os.listdir(pathlib.Path(NOVELS_DIR))
+
+print(novelas)
 stemmer = SnowballStemmer("english")
-tokenizer = StanfordTokenizer()
+lemmer = WordNetLemmatizer()
 sw = stopwords.words('english')
-with open("Novelas_en_formato_txt/farenheit_prepro.txt") as f:
-    for line in f:
-        tokens = tokenizer.tokenize(line.strip())
-        nuevos = [stemmer.stem(token) for token in tokens if token not in sw]
-        print(' '.join(nuevos))
+
+
+def novel_words (novel, lemmatizer, sw):
+    for line in novel:
+        line = line.strip()
+        tokens = (w for w in tokenize(line) if w not in sw)
+        nuevos = (lemmer.lemmatize(tok) for tok in tokens if tok.isalpha())
+        yield ' '.join(nuevos)
